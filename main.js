fe0ece14a25ea4f48104a9353e57baefa5ea852b
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2021-12-06 17:52:20 +0530

    update main.js
    Added
    1. Check palindrome
    2. Date to String

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
-const bday = document.querySelector('#bday');
-const checkBtn = document.querySelector('#check-btn');
+const bday = document.querySelector("#bday");
+const checkBtn = document.querySelector("#check-btn");
 
-function reverseString(str){
-    var revStr = str.split('').reverse().join('');
-    console.log(revStr);
+function reverseString(str) {
+  var revStr = str.split("").reverse().join("");
+  return revStr;
 }
 
-// checkBtn.addEventListener('click', reverseString(str));
+function isPalindrome(str) {
+  var revStr = reverseString(str);
+  return str === revStr;
+}
+
+function getDateAsString(date) {
+    var dateInStr = { day:"",month:"",year:""};
+    //day
+    if(date.day<10){
+        dateInStr.day="0"+date.day;
+    }else{
+        dateInStr.day = date.day.toString();
+    }
+    //month
+    if(date.month<10){
+        dateInStr.month="0"+date.month;
+    }else{
+        dateInStr.month = date.month.toString();
+    }
+    //year
+    dateInStr.year = date.year.toString();
+    console.log(dateInStr);
+}
+
+var date = { day: 14, month: 9, year: 2020 };
+
+checkBtn.addEventListener("click", getDateAsString(date));
