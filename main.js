cc4f210ee0906742695624c6abc4feab15d202d4
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2021-12-06 17:14:19 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+const bday = document.querySelector('#bday');
+const checkBtn = document.querySelector('#check-btn');
+
+checkBtn.addEventListener('click', function clicked(){
+    console.log("clicked");
+});
