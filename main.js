0e40e648d9a0c78efb83aea8ba4e6596f707c7dc
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2021-12-06 19:16:44 +0530

    update main.js
    added next date function

--- main.js
+++ main.js
@@ -48,29 +48,68 @@ function getDateInAllFormats(date) {
 
 //check palindrome for all date formats obtained from above using isPalindrome fn.
 function checkPalindrome(date) {
-    var dateList = getDateInAllFormats(date);
-    var palindromeList = [];
+  var dateList = getDateInAllFormats(date);
+  var palindromeList = [];
+
+  for (var i = 0; i < dateList.length; i++) {
+    var result = isPalindrome(dateList[i]);
+    palindromeList.push(result);
+  }
+  return palindromeList;
+}
+
+//if none of the dates are palindrome, find the next palindrome date
+
+//check for leap year
+function isLeapYear(year) {
+  if (year % 400 === 0) return true;
+
+  if (year % 100 === 0) return false;
+
+  if (year % 4 === 0) return true;
+
+  return false;
+}
+
+//get the next day's date
+function getNextDate(date) {
+    var day = date.day + 1;
+    var month = date.month;
+    var year = date.year;
   
-    for (var i = 0; i < dateList.length; i++) {
-      var result = isPalindrome(dateList[i]);
-      palindromeList.push(result);
+    var daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  
+    if (month === 2) {
+      if (isLeapYear(year)) {
+        if (day > 29) {
+          day = 1;
+          month++;
+        }
+      } else {
+        if (day > 28) {
+          day = 1;
+          month++;
+        }
+      }
+    } else {
+      if (day > daysInMonth[month - 1]) {
+        day = 1;
+        month++;
+      }
     }
-    return palindromeList;
+  
+    if (month > 12) {
+      month = 1;
+      year++;
+    }
+  
+    console.log ({
+      day: day,
+      month: month,
+      year: year,
+    });
   }
-
-//if none of the dates are palindrome
-  //check for leap year
-  function isLeapYear(year) {
-    if (year % 400 === 0) return true;
   
-    if (year % 100 === 0) return false;
-  
-    if (year % 4 === 0) return true;
-  
-    return false;
-  }
-
-  //
 var date = { day: 10, month: 9, year: 2020 };
 
-checkBtn.addEventListener("click", checkPalindrome(date));
+checkBtn.addEventListener("click", getNextDate(date));
